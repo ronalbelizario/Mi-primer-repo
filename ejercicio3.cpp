ea4de29e73bc5d6ca64f621919869c0e4637f17e
ronalbelizario <rodriguezalberth111@gmail.com>
2026-09-14 17:29:18 -0500

    Add files via upload
    ejercicio3

---
+++ ejercicio3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int A = 5, B =10;
+    A = 3;
+    B = A + B;
+    cout << "A=" << A << endl;
+    cout << "B=" << B << endl;
+    return 0;
+}
